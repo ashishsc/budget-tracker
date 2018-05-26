21eab24f0c7ca817434bcfd8f331bedf7a3d77fc
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2018-05-26 18:49:49 -0400

    basic scotty app

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -1,4 +1,10 @@
 module Main where
 
-main :: IO ()
-main = pure ()
+import Data.Monoid (mconcat)
+import Web.Scotty
+
+main =
+  scotty 3000 $
+  get "/:word" $ do
+    beam <- param "word"
+    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
