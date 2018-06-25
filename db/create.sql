84e9c36f8d4bc5a61f488abbe465318a2e9d75de
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2018-06-25 20:54:48 +0800

    Update schema to allow empty categories

--- db/create.sql
+++ db/create.sql
@@ -2,9 +2,13 @@ CREATE TABLE user (
     username varchar(255) PRIMARY KEY
 )
 
+CREATE TABLE category (
+    name varchar(255) PRIMARY KEY
+)
+
 CREATE TABLE expense (
     id INT PRIMARY KEY,
-    category varchar(45),
+    category varchar(255) REFERENCES category(name),
     name varchar(45),
     value int,
     user varchar(255) REFERENCES user(username)
